3a2baa26e0aaed0f6e7c0e7a65ea63b1e06ee77b
Rahul Kumar <kumar.coding.practice@gmail.com>
2024-09-28 22:37:07 +0530

    search bar

--- frontend/src/components/QuesTemplate/TopicQuesTemplate.jsx
+++ frontend/src/components/QuesTemplate/TopicQuesTemplate.jsx
@@ -49,6 +49,8 @@ const TopicQuesTemplate = () => {
   const [markedQuestions, setMarkedQuestions] = useState({});
   // State to track checkbox completion for each question
   const [completedQuestions, setCompletedQuestions] = useState({});
+  // search questions
+  const [searchQues, setSearchQues] = useState("");
 
   const userId = JSON.parse(localStorage.getItem("userId"));
 
@@ -174,12 +176,50 @@ const TopicQuesTemplate = () => {
     }
   };
 
+  const searchQuestion = async (val) => {
+    const searchQues = await axios.get(
+      `http://localhost:8000/api/questions/get-${topic}-questions`
+    );
+
+    const ldata = searchQues.data;
+    const fdata = [];
+
+    ldata.map((li) => {
+      if (li.question.toLowerCase().includes(val)) {
+        fdata.push(li);
+        setQues(fdata);
+      }
+    });
+  };
+
   return (
     <>
       <div className="bg w-full min-h-screen text-slate-50 selection:bg-[#ffbe25db] selection:text-slate-50">
         <div className="px-4 sm:px-5 md:px-14">
           <Navbar />
         </div>
+
+        <div className="px-4 sm:px-5 md:px-14 py-6">
+          {/* Serach Box */}
+          <div className="flex md:flex-row flex-col gap-6">
+            <input
+              type="search"
+              className="inline-block py-2 px-6 w-full md:w-[90%] bg-transparent border rounded-md outline-none placeholder:text-slate-400 placeholder:font-medium"
+              value={searchQues}
+              placeholder="Eg: reverse"
+              onChange={(e) => setSearchQues(e.target.value)}
+            />
+            <div className="flex items-center justify-center">
+              <button
+                className="inline-block border-[2px] px-2 md:py-1 rounded-md text-lg font-semibold hover:text-zinc-950 hover:bg-[#ffbd25] transition-all duration-150 xxs:w-[30%] md:w-[100%]"
+                onClick={() => searchQuestion(searchQues)}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        
         {!loading ? (
           <div className="px-4 sm:px-5 md:px-14 py-6 relative">
             <div className="w-full bg-[#333] overflow-x-scroll lg:overflow-x-hidden">
